0f768ce25d22fdbbe42340041013e378f840e497
qiao277 <suyaqiao42@gmail.com>
2025-08-05 16:55:12 +0800

    some
    1234

--- backend/routes/qiao.js
+++ backend/routes/qiao.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 require('../moudle/index'); // 确保用户模型被加载
-
+var { Product } = require('../moudle/goods');
 /* GET home page. */
-router.get('/products', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+// router.get('/products', function (req, res, next) {
+//     res.render('index', { title: 'Express' });
+// });
+// 获取商品列表
+
+
+router.get('/products', async function (req, res, next) {
+
+    const products = await Product.find({});
+
+
+
+
+
 });
+
 
 module.exports = router;
